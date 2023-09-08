963a1a0500d61f010ad1bfa87c66b1cd66b39c29
GamerBhai02 <101631188+GamerBhai02@users.noreply.github.com>
2023-09-08 10:02:58 +0530

    Update url_shortner.py

--- plugins/helper/url_shortner.py
+++ plugins/helper/url_shortner.py
@@ -5,10 +5,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from pyrogram.handlers import MessageHandler
 from pyshorteners import Shortener
 
-BITLY_API = os.environ.get("BITLY_API", "8df1df8c23f719e5cf97788cc2d40321ea30092b")
-CUTTLY_API = os.environ.get("CUTTLY_API", "f64dffbde033b6c307387dd50b7c76e505f1c")
-SHORTCM_API = os.environ.get("SHORTCM_API", "pk_...NIZv")
-GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9007a7")
+BITLY_API = os.environ.get("BITLY_API", "68bb1de6173a17c598d0032c6295f99e7bc3cc0c")
+CUTTLY_API = os.environ.get("CUTTLY_API", "4e46f6ceaa53267b4d31a0a4f9fc4ad68ce75")
+SHORTCM_API = os.environ.get("SHORTCM_API", "pk_hGf8Xqhgxu6G5yBa")
+GPLINKS_API = os.environ.get("GPLINKS_API", "d3f34f17972e981b5a962275a730c954523dd34b")
 
 reply_markup = InlineKeyboardMarkup(
         [[
